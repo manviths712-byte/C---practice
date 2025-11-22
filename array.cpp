35c9b1f286db09283ffcb81cd26d5374405beab1
Manvith s <manviths712@gmail.com>
2025-11-22 21:43:03 +0530

    Add files via upload
    👉Introduction to  array

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    int nums[]={1,2,3};
+    int marks[5]={90,34,33,46,97};
+    
+    
+    cout<<marks[0]<<endl;
+    
+    return 0;
+}
