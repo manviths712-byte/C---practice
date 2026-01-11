e143847ce30af7811c123f0a71378a94516741f7
Manvith s <manviths712@gmail.com>
2026-01-11 22:59:33 +0530

    Add files via upload
    Added palindrome string program using C++

---
+++ string_palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int start = 0;
+    int end = str.length() - 1;
+    bool isPalindrome = true;
+
+    while(start < end) {
+        if(str[start] != str[end]) {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    if(isPalindrome)
+        cout << "Palindrome String";
+    else
+        cout << "Not a Palindrome";
+
+    return 0;
+}
