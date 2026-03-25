76da1ec78d79b8a7356d225a2f6d64ed4d16f161
Manvith s <manviths712@gmail.com>
2026-03-25 20:51:27 +0530

    Function Overriding (Polymorphism)

---
+++ oops/function_overriding.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+public:
+    void eat(){
+        cout<<"Animal eats food"<<endl;
+    }
+};
+
+class Dog : public Animal{
+public:
+    void bark(){
+        cout<<"Dog barks"<<endl;
+    }
+};
+
+int main(){
+
+    Dog d1;
+
+    d1.eat();
+    d1.bark();
+
+    return 0;
+}
